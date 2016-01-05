fc0ce4f4d07ee9604010f62d43b07759c0e8838d
james.payne <jepayne1138@gmail.com>
2016-01-05 14:53:11 -0600

    Add framework for supporting delta compression of updated object files

--- process.py
+++ process.py
@@ -41,7 +41,7 @@ class VersionControl:
     """
 
     VC_DIR = '.vc'
-    BLOBMAP = 'blobmap'
+    HASHMAP = 'hashmap'
 
     def __init__(self, root, create=False):
         """Simple store all arguments and call main initialization method"""
@@ -56,7 +56,7 @@ class VersionControl:
         # Define paths to important locations
         self.vc_dir = self.VC_DIR
         self.obj_dir = os.path.join(self.vc_dir, 'objects')
-        self.blobmap = os.path.join(self.vc_dir, self.BLOBMAP)
+        self.hashmap_path = os.path.join(self.vc_dir, self.HASHMAP)
 
         if self.create and not os.path.isdir(self.vc_dir):
             self.create_directory()
@@ -72,6 +72,9 @@ class VersionControl:
             raise ValueError(
                 'Version control integrity error: {}'.format(self.root)
             )
+
+        # Define helper attributes
+        self.hashmap = {}
 
     @root_directory
     def create_directory(self):
@@ -116,7 +119,7 @@ class VersionControl:
         node_content = '\n'.join(node_entries) + '\n'
 
         # Save the node contents to a vc object
-        return self._save_node(node_content)
+        return self._save_node(directory, node_content)
 
     def _create_blob_node(self, path):
         """Creates nodes for files in the current snapshot"""
@@ -124,18 +127,12 @@ class VersionControl:
             node_content = input_file.read()
 
         # Save the node contents to a vc object
-        return self._save_node(node_content)
+        return self._save_node(path, node_content)
 
-    def _save_node(self, node_content):
+    def _save_node(self, path, node_content):
         """Calculates a content hash and saves the content to a file"""
         # Get node content hash
-        sha1 = hashlib.sha1()
-        if type(node_content) is bytes:
-            bin_content = node_content
-        else:
-            bin_content = node_content.encode()
-        sha1.update(bin_content)
-        digest = sha1.hexdigest()
+        digest = self._hash_diget(node_content)
 
         # Parse object directory and filename
         obj_dir = os.path.join(self.obj_dir, digest[:2])
@@ -148,12 +145,83 @@ class VersionControl:
             with open(obj_path, 'wb') as obj_file:
                 obj_file.write(bin_content)
 
+        self._update_hashmap(path, digest, node_content)
+
         return digest
+
+    def _hash_diget(self, payload):
+        """Returns a hex digest for the hash of the given payload"""
+        hasher = hashlib.sha1()
+        # Convert to binary content if necessary
+        if type(node_content) is bytes:
+            bin_content = node_content
+        else:
+            bin_content = node_content.encode()
+        hasher.update(bin_content)
+        return hasher.hexdigest
 
     def _create_blobcache(self):
         """Creates a new empty file that will store current file hashes"""
-        with open(self.blobmap, 'wb') as blobfile:
-            pickle.dump({}, blobfile)
+        with open(self.hashmap_path, 'wb') as hashmap_file:
+            pickle.dump({}, hashmap_file)
+
+    def _update_hashmap(self, obj_path, obj_hash, obj_content):
+        """Updates the hashmap with a pointer to the new file"""
+        if path in self.hashmap:
+            if self.hashmap[path] != obj_hash:
+                self._delta_compress(self.hashmap[path], obj_hash, obj_content)
+        # Update hashmap
+        self.hashmap[path] == obj_hash
+
+    def _delta_compress(self, old_hash, new_hash, new_content):
+        """Compresses an old object file by replacing with a delta
+
+        NOTE:
+          The file name hash no longer will reflect the true file
+          content, rather the content that the delta reflects.
+        """
+        # Clean up old file
+        old_dir = os.path.join(self.obj_dir, old_hash[:2])
+        old_path = os.path.join(old_dir, old_hash[2:])
+
+        # Calculate delta from new to old version
+        delta = ''
+
+        # Format delta contents
+        delta_dict = {
+            'origin': new_hash,
+            'delta': delta,
+        }
+        delta_contents = pickle.dumps(delta_dict)
+
+        # Overwrite old file contents with a delta to the new file
+        with open(old_path, 'wb') as obj_file:
+            obj_file.write(delta_contents)
+
+    def _read_object(self, obj_hash):
+        """Reads and returns the contents of an object file with given hash
+
+        Recursively rebuilds any necessary files from their deltas
+        """
+        obj_dir = os.path.join(self.obj_dir, obj_hash[:2])
+        obj_path = os.path.join(old_dir, obj_hash[2:])
+
+        with open(obj_path, 'rb') as obj_file:
+            content = obj_file.read()
+
+        # Check if a delta by comparing the content to the hash value
+        if obj_hash != self._hash_diget(content):
+            # Delta object must be rebuilt
+            delta_dict = pickle.loads(content)
+            origin_content = self._read_object(delta_dict['origin'])
+            return self._rebuild_content(origin_content, delta_dict['delta'])
+
+        # If object is not a delta, simply return it's content
+        return content
+
+    def _rebuild_content(self, origin, delta):
+        """Rebuilds object content from origin data and a delta"""
+        return ''
 
 
 def main():
