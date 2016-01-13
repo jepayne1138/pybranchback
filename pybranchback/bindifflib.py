25e02fb2ae1a7d0390ba053fa5139dd09edc4926
james.payne <jepayne1138@gmail.com>
2016-01-13 15:03:10 -0600

    Add framework for binary diff module

---
+++ pybranchback/bindifflib.py
@@ -0,0 +1,11 @@
+"""Library for performing binary delta compressions"""
+
+
+def diff(compress, reference):
+    """Compresses the given bytes using the reference and return a patch"""
+    pass
+
+
+def patch(patch, reference):
+    """Return an uncompressed file by applying the patch to the reference"""
+    pass
