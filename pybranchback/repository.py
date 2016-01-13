7c237ed2fda4e0b2acbe5062f10ee63bed1e08a0
james.payne <jepayne1138@gmail.com>
2016-01-13 16:44:52 -0600

    In progress validation and creation methods

--- pybranchback/repository.py
+++ pybranchback/repository.py
@@ -19,10 +19,44 @@ class Repository:
     |  snapshots
     """
 
+    REQUIRED_DIRS = [
+        '.pbb',
+        '.pbb/objects',
+        '.pbb/refs',
+        '.pbb/refs/heads',
+    ]
+    REQUIRED_FILES = [
+        '.pbb/objhashcache',
+        '.pbb/HEAD',
+        '.pbb/snapshots',
+    ]
+
     def __init__(self, root_dir, create=False):
         """Initialize instance variables"""
         self.root_dir = root_dir
         self.create = create
+
+        # Validate that a repository exists at the given location
+        if not self.validate_repo():
+            if self.create:
+                self.create_repo()
+            else:
+                raise ValueError('Repository does not exist or is invalid')
+
+    def validate_repo(self):
+        """Check that the repository structure exists and is valid"""
+        def is_dir(relative_path):
+            return True
+        def is_file(relative_path):
+            return True
+        return (
+            all(map(os.path.isdir, self.REQUIRED_DIRS)) and
+            all(map(os.path.isfile, self.REQUIRED_FILES))
+        )
+
+    def create_repo(self):
+        """Create a new repository directory in the root location"""
+        pass
 
 
 # Helper functions for listing in a file structure with a blacklist
